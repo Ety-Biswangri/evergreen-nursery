95dba25aa958a3592744299da992baf24f0cf8db
Ety-Biswangri <etybgc02@gmail.com>
2022-03-31 01:05:06 +0600

    all done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import ReactConcepts from './components/ReactConcepts/ReactConcepts';
 import Shop from './components/Shop/Shop';
 
 function App() {
